5bdf095d15e6eb7c218975a0973e32fc72c31a5d
Dmitry Semenov <lonelyteapot@gmail.com>
2024-03-25 22:48:21 +0300

    Prompt to grant permissions

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,13 +1,37 @@
+const requiredPermissions = {
+    origins: [
+        "*://*.youtube.com/*",
+        "*://yt3.ggpht.com/*"
+    ]
+}
+
+let openedTabs = [];
+
 function handleClick() {
     console.debug("Requesting permissions")
-    chrome.permissions.request({
-        origins: [
-            "*://*.youtube.com/*",
-            "*://yt3.ggpht.com/*"
-        ]
+    chrome.permissions.request(requiredPermissions, () => {
+        const lastTab = openedTabs.pop()
+        chrome.tabs.update(lastTab, {
+            url: "https://www.youtube.com",
+        });
+        chrome.tabs.remove(openedTabs);
+        openedTabs.length = 0;
     })
 }
 
-chrome.action.onClicked.addListener(handleClick);
+function checkPermissions() {
+    chrome.permissions.contains(requiredPermissions, (hasPermissions) => {
+        if (!hasPermissions) {
+            console.debug("Missing permissions")
+            chrome.tabs.create({url: "missing_permissions.html", active: true}, (tab) => {;
+                openedTabs.push(tab.id);
+            });
+        }
+    });
+}
 
-console.debug("Add-on loaded");
+chrome.action.onClicked.addListener(handleClick);
+chrome.permissions.onRemoved.addListener(checkPermissions);
+
+console.debug("Extension loaded");
+checkPermissions()
